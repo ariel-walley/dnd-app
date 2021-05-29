61df1fb1c8897709b8b59ea33fdeabf2ee3e0fe8
Emily Carlsen <emilytcarlsen@gmail.com>
2021-05-29 16:29:51 -0400

    Add server code skeleton

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
