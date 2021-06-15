7004c39f2169b6cab2b6f107c313598e896a36b1
Ariel Walley <arielwalley95@gmail.com>
2021-06-15 16:01:40 -0400

    Name change (previously imitator.js)

---
+++ client/src/components/snapshots.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Snapshots extends React.Component {
+  render() {
+    return(
+      <div>
+        <div>Imitate page</div>
+      </div>
+
+    )
+  }
+}
+
+export default Snapshots;
