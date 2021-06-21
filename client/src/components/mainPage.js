91aebdb985291b27f5414e66cb57ab74422aefb4
Ariel Walley <arielwalley95@gmail.com>
2021-06-21 14:38:31 -0400

    Convert MainPage from class to functional component
    - Also changed name from main.js to mainPage.js

--- client/src/components/mainPage.js
+++ client/src/components/mainPage.js
@@ -3,16 +3,14 @@ import History from './history';
 import Snapshots from './snapshots';
 import ContentContainer from './content_components/contentContainer';
 
-class MainPage extends React.Component {
-  render() {
-    return(
-      <div>
-        <History/>
-        <Snapshots/>
-        <ContentContainer/>
-      </div>
-    )
-  }
+function MainPage() {
+  return(
+    <div>
+      <History/>
+      <Snapshots/>
+      <ContentContainer/>
+    </div>
+  )
 }
 
 export default MainPage;
