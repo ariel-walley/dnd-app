0b92c4bc620950ec9e5c3d7d92e2470e3cb4c879
Ariel Walley <arielwalley95@gmail.com>
2021-10-22 08:34:34 -0400

    Create outline of snapshots

--- client/src/components/controlPanel/snapshots.js
+++ client/src/components/controlPanel/snapshots.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PlayersContext from '../../context';
+
+import styled from 'styled-components';
+import { Container } from '../../styles';
+
+const SnapshotTitle = styled.h1`
+  width: 100%;
+  text-align: center;
+  font-size: 25px;
+`;
+
+const SnapshotContainer = styled(Container)`
+  border: grey solid 1px;
+  padding: 5px;
+`;
+
+const PlayerSnapshot = styled.div`
+  height: 100px;
+  width: 100px;
+  border: white solid 1px;
+  margin: 5px;
+  display: flexbox;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  
+`;
 
 function Snapshots() {
+  const { players } = useContext(PlayersContext);
+
+  const generateSnapshots = Object.values(players).map((player) => 
+      <PlayerSnapshot key={player + 'Snapshot'}>{player}</PlayerSnapshot>
+  )
+
   return(
-    <div>
-      <div>Imitate page</div>
-    </div>
+    <Container>
+      <SnapshotTitle>Snapshots</SnapshotTitle>
+      <SnapshotContainer>
+        {generateSnapshots}
+      </SnapshotContainer>
+    </Container>
   );
 }
 
